aec40554b2089e410b8bee4b485a04067ec5299f
Natasha Edun <natashaedun@Natashas-MacBook-Pro-2.local>
2022-07-28 22:49:52 +0100

    resolving git merge conflict

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 HOST = "localhost"
 USER = "root"
-PASSWORD = "T0M0RR0W_1"
+PASSWORD = "" #set your own password here
+DATABASE_NAME = "exchange_project"
 
 # update password with your sql password
 # pip3 install mysql-connector-python to get database and python to link up
