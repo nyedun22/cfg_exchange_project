c6926e6234f727b75fcbde877ab2921a119b6a10
Natasha Edun <natashaedun@Natashas-MacBook-Pro-2.local>
2022-07-28 23:10:26 +0100

    app.py

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
-from flask import Flask, jsonify, request
-
 # run app file to view flask site
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+app.config["SECRET_KEY"] = "12345678"
 
-# creating app routes -- example used for now
+
 @app.route('/')
 def hello_world():
     return jsonify({'hello':'universe'})
 
 
+# route for currency convertor
+@app.route('/currency')
+def currency_convertor():
+    return jsonify({'convert': 'currency'})
+
+
+# route for user sign up form
+@app.route('/register')
+def user_sign_up():
+    form = CustomerRegistrationForm()
+    return render_template('register.html', form=form)
+
+
+# route for viewing transactions page
+@app.route('/transactions')
+def transactions():
+    return jsonify({'my': 'transactions'})
+
 
 if __name__ == '__main__':
-     # app.run(debug=True, host='0.0.0.0')
-     app.run(debug=True, port=5003)
-
+    # app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, port=5003)
