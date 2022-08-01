08cc9f492ddfa16822bef03104ac245b59f61352
Natasha Edun <natashaedun@Natashas-MacBook-Pro-2.local>
2022-08-01 11:29:37 +0100

    form validation - char lengths for text input fields

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, jsonify, render_template, request, flash
 from forms import CustomerRegistrationForm, LoginForm
 from currency_codes import get_currencies
-from db_setup import mydb
+from db_setup import mydb, cursor
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "12345678"
@@ -24,11 +24,6 @@ def currency_convertor():
 def user_sign_up():
     form = CustomerRegistrationForm()
     # validation for new customer registration form
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-    else:
-        return render_template('register.html', form=form)
-
     if request.method == 'POST':
         username = request.form['username'],
         password = request.form['password'],
@@ -57,16 +52,59 @@ def user_sign_up():
                 first_name=first_name,
                 last_name=last_name,
                 email=email,
-                address_line_one=address_line_one,
+                address=address_line_one,
                 postcode=postcode,
                 user_login=user_login)
 
-            cur = mysql.connection.cursor()
+            # cur = mysql.connection.cursor()
             mydb.session.add(user_login)
             mydb.session.add(user_sign_up)
             mydb.session.commit()
-            return render_template('home.html', title='Home', message=error, form=form)
+            return render_template('home.html', title='Home', form=form)
         return render_template('register.html', title='Register', message=error, form=form)
+
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+    else:
+        return render_template('register.html', form=form)
+
+    if request.method == 'POST':
+        username = request.form['username'],
+        password = request.form['password'],
+        first_name = request.form['first_name'],
+        last_name = request.form['last_name'],
+        email = request.form['email'],
+        address_line_one = request.form['address'],
+        postcode = request.form['postcode']
+
+        """if/else statement - if form field input text length less than required user will get error
+        otherwise user data will submit to database tables"""
+        if len(first_name) == 0 \
+                or len(last_name) == 0 \
+                or len(email) == 0\
+                or len(address_line_one) == 0\
+                or len(postcode) == 0\
+                or len(password) < 4\
+                or len(username) == 0:
+            error = "Please complete each section of this form"
+        else:
+            print("x")
+        #     mydb.user_login = user_sign_up(username=username, pass_word=pass_word)
+        #
+        #     mydb.user_details = user_sign_up(
+        #         first_name=first_name,
+        #         last_name=last_name,
+        #         email=email,
+        #         address_line_one=address_line_one,
+        #         postcode=postcode,
+        #         user_login=user_login)
+        #
+        #     # cur = mysql.connection.cursor()
+        #     mydb.session.add(user_login)
+        #     mydb.session.add(user_sign_up)
+        #     mydb.session.commit()
+        #     return render_template('home.html', title='Home', form=form)
+        # return render_template('register.html', title='Register', message=error, form=form)
 
 # route for user login form
 @app.route('/login')
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 HOST = "localhost"
 USER = "root"
-PASSWORD = "" #set your own password here
+PASSWORD = "T0m0rr0w1" #set your own password here
 DATABASE_NAME = "exchange_project"
 
 # pip3 install mysql-connector-python to get database and python to link up
